f956b010ee8825bf8ebd24dbbf240b0a6c22bdf1
Nanio <111894274+NanioY@users.noreply.github.com>
2023-10-02 10:01:37 +0200

    feat: add Island import and button functionality

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-// kickstart your world here
+import Island from './classes/Island.js';
+
+const btnAddIsland = document.getElementById('btnAddIsland');
+
+btnAddIsland.addEventListener('click', () => {
+    const island = new Island();
+    island.add();
+});
