b8c0bc494fac69933b363e1adb0e4e3665aa6168
Nanio <111894274+NanioY@users.noreply.github.com>
2023-10-03 10:22:42 +0200

    feat: remove 'add' function from island.js

--- js/classes/Island.js
+++ js/classes/Island.js
@@ -1,5 +1,8 @@
 export default class Island {
-  constructor(name) { }
+  constructor(name, color) { 
+    this.name || this.getRandomName();
+    this.color || this.getRandomColor();
+  }
 
   getRandomColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
@@ -26,50 +29,5 @@ export default class Island {
     ];
 
     return names[Math.floor(Math.random() * names.length)];
-  }
-
-  add() {
-    // create a new element
-    const island = document.createElement("div");
-
-    // add a class to the element
-    island.classList.add("island");
-
-    // add a random name to the element
-    island.innerHTML = this.getRandomName();
-
-    // add a random color to the element
-    island.style.backgroundColor = this.getRandomColor();
-
-
-    // calculate position within 70% of the viewport width and height, including negative values
-    const viewportWidth = window.innerWidth * 0.9;
-    const viewportHeight = window.innerHeight * 0.9;
-    const randomX = (Math.random() - 0.5) * viewportWidth;
-    const randomY = (Math.random() - 0.5) * viewportHeight;
-
-    // animate the element to a random position
-    island.animate(
-      [
-        {
-          // start position in center of the screen
-          transform: "translate(-50%, -50%)"
-        },
-        {
-          // random end position within 70% of the viewport
-          transform: `translate(${randomX}px, ${randomY}px)`
-
-        }
-      ],
-      {
-        // timing options
-        duration: 1000,
-        iterations: 1,
-        fill: "forwards"
-      }
-    );
-
-    // add the element to the DOM
-    document.body.appendChild(island);
   }
 }
