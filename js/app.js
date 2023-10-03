d90c9081b544f9ea4584abcfd88c5393cb3630d3
Yannick Van den Brande <yavadebr@gmail.com>
2023-10-03 13:30:10 +0200

    feat: initialize World class and set up event listeners

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 import World from './classes/World.js';
 
 const world = new World();
+
+const btnAddIsland = document.getElementById('btnAddIsland');
+const btnSave = document.getElementById('btnSave');
+const btnLoad = document.getElementById('btnLoad');
+
+btnAddIsland.addEventListener('click', () => {
+  world.addIsland();
+});
+
+btnSave.addEventListener('click', () => {
+  world.save();
+});
+
+btnLoad.addEventListener('click', () => {
+  world.load();
+});
